e24da3456e6a06724b27eeae9a7f34101b9146bd
Lorenzo Cutrera <lorenzoq3ra@gmail.com>
2023-09-20 18:24:34 +0200

    add complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,34 @@
+/*
+1 - Crea una variabile showList impostata a false inizialmente
+2 - Dopo il cilco imposta this.showList a true
+3 - nel ul inserisci un v-if="showList"
+*/
+
+
 
 const { createApp } = Vue
 
-  createApp({
+createApp({
     data() {
-      return {
-        message: 'Hello Vue!'
-        randomMail: ''
-      }
-    }
-    mounted() {
-        get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(response => {
-                console.log(response);
-                this.randomMail.push(response.data.response);
-                
-            })
+        return {
+            message: 'Hello Vue!',
+            randomMails: [],
+            showList: false
         }
-        
-      }
-  ).mount('#app')
+    },
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(response => {
+                    console.log(response);
+                    this.randomMails.push(response.data.response);
+
+                })
+        }
+
+        this.showList = true
+    }
+
+}
+
+).mount('#app')
